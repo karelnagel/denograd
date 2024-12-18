effc44d8ad0bf89fc4f529a302ac18ca067e36f5
Karel <nagelkarel@gmail.com>
2024-12-18 11:04:37 +0200

    save

--- test/engine/search.test.ts
+++ test/engine/search.test.ts
@@ -1,67 +1,67 @@
 import { compare } from '../helpers.ts'
 
-Deno.test(
-  'actions',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   'actions',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
-Deno.test(
-  '_get_test_global_size',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   '_get_test_global_size',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
-Deno.test(
-  '_time_program',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   '_time_program',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
-Deno.test(
-  '_try_compile_linearized_w_idx',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   '_try_compile_linearized_w_idx',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
-Deno.test(
-  'bufs_from_lin',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   'bufs_from_lin',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
-Deno.test(
-  'get_kernel_actions',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   'get_kernel_actions',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
-Deno.test(
-  'beam_search',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   'beam_search',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
 
 Deno.test(
   'optimize_local_size',
@@ -72,11 +72,11 @@ Deno.test(
   ),
 )
 
-Deno.test(
-  'time_linearizer',
-  compare(
-    [],
-    () => {},
-    'out(XXX)',
-  ),
-)
+// Deno.test(
+//   'time_linearizer',
+//   compare(
+//     [],
+//     () => {},
+//     'out(XXX)',
+//   ),
+// )
