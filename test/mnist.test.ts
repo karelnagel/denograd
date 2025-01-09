64dd78d68c15e283c26236c1447fa2795d96226d
Karel <nagelkarel@gmail.com>
2025-01-09 17:58:48 +0200

    mnist.train test

--- test/mnist.test.ts
+++ test/mnist.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'expect/expect'
 import { MNIST } from '../beautiful_mnist.ts'
-import { load_state_dict, mnist, safe_load } from '../src/mod.ts'
+import { Adam, get_parameters, load_state_dict, mnist, safe_load } from '../src/mod.ts'
 import { Tensor } from '../src/tensor.ts'
 import { asdict, compare, python } from './helpers.ts'
 
@@ -65,4 +65,44 @@ Deno.test(
     ])
     expect(await asdict(ts)).toEqual(await asdict(py))
   },
+)
+
+Deno.test(
+  'mnist.train',
+  compare(
+    [[]],
+    async () => {
+      Tensor.manual_seed(333)
+      const [x_train, y_train] = await mnist()
+      const model = new MNIST()
+      const opt = Adam(get_parameters(model))
+
+      Tensor.training = true
+      opt.zero_grad()
+      const samples = new Tensor([1])
+      const loss = model.call(x_train.get(samples)).sparse_categorical_crossentropy(y_train.get(samples)).backward()
+      // await opt.step()
+      Tensor.training = false
+
+      return loss
+    },
+    [
+      'from tinygrad import nn, Tensor',
+      'from examples.beautiful_mnist import Model',
+
+      'Tensor.manual_seed(333)',
+      'x_train, y_train, _, _ = nn.datasets.mnist()',
+      'model = Model()',
+      'opt = nn.optim.Adam(nn.state.get_parameters(model))',
+
+      'Tensor.training = True',
+      'opt.zero_grad()',
+      'samples = tiny.Tensor([1])',
+      'loss = model(x_train[samples]).sparse_categorical_crossentropy(y_train[samples]).backward()',
+      'opt.step()',
+      'Tensor.training=False',
+
+      'out(loss)',
+    ],
+  ),
 )
