a94dcd77b068a7a1ecec34c23610374b93d637c8
Karel <nagelkarel@gmail.com>
2025-01-15 16:20:56 +0200

    mnist infer working with astro

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://karelnagel.github.io',
     base: 'denograd',
+    integrations: [react(), tailwind()],
+    devToolbar: { enabled: false }
 });
