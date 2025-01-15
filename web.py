e6ce61318305051ad769afd43aa1bb48bd1fa791
Karel <nagelkarel@gmail.com>
2025-01-15 15:48:32 +0200

    remove web...

--- web.py
+++
@@ -1,7 +0,0 @@
-from tinygrad.tensor import Tensor
-
-
-a = Tensor([5])
-b = Tensor([4])
-
-print(a.add(b).tolist())
--- web.ts
+++
@@ -1,6 +0,0 @@
-import { Tensor } from './denograd/tensor.ts'
-
-const a = new Tensor([2.3])
-const b = new Tensor([6.4])
-
-console.log(await a.div(b).add(4).tolist())
