8992b2885f8210b61a002b2c0bb315afc1f15c4e
Karel <nagelkarel@gmail.com>
2025-01-15 16:45:51 +0200

    save

--- website/src/components/Canvas.tsx
+++ website/src/components/Canvas.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
+import { is_eq } from '../../../denograd/helpers.ts'
 
 type Image = number[][]
 export const Canvas = ({ image, setImage }: { image: Image; setImage: (img: Image) => void }) => {
@@ -14,30 +15,12 @@ export const Canvas = ({ image, setImage }: { image: Image; setImage: (img: Imag
     ctx.strokeStyle = 'black'
   }, [])
 
+  // Used for clearing the canvas only
   useEffect(() => {
+    if (!is_eq(image, Array(28).fill(0).map(() => Array(28).fill(0)))) return
     const ctx = canvasRef.current!.getContext('2d')
     ctx.fillStyle = 'white'
     ctx.fillRect(0, 0, 280, 280)
-    
-    // Scale up the 28x28 image to 280x280
-    const tempCanvas = document.createElement('canvas')
-    tempCanvas.width = tempCanvas.height = 28
-    const tempCtx = tempCanvas.getContext('2d')!
-    const imageData = tempCtx.createImageData(28, 28)
-    
-    for (let y = 0; y < 28; y++) {
-      for (let x = 0; x < 28; x++) {
-        const i = (y * 28 + x) * 4
-        const value = 255 - image[y][x]
-        imageData.data[i] = value
-        imageData.data[i + 1] = value
-        imageData.data[i + 2] = value
-        imageData.data[i + 3] = 255
-      }
-    }
-    
-    tempCtx.putImageData(imageData, 0, 0)
-    ctx.drawImage(tempCanvas, 0, 0, 280, 280)
   }, [image])
 
   const updateImage = () => {
