623376d171b19f7bc33202b9c216f2774fdf50ae
Karel <nagelkarel@gmail.com>
2025-01-15 14:09:18 +0200

    save

--- examples/react/main.tsx
+++ examples/react/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode, useEffect, useRef, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Tensor } from '@denograd/denograd'
+import { is_eq, Tensor } from '@denograd/denograd'
 import { MNIST } from '@denograd/models'
 
 const mnist = new MNIST()
@@ -27,7 +27,7 @@ const Canvas = ({ image, setImage }: { image: Image; setImage: (img: Image) => v
     const scx = small.getContext('2d')!
     scx.drawImage(big, 0, 0, 28, 28)
     const d = scx.getImageData(0, 0, 28, 28).data
-    const newImg = EMPTY
+    const newImg = Array(28).fill(0).map(() => Array(28).fill(0))
     for (let i = 0; i < d.length; i += 4) {
       const x = (i / 4) % 28, y = Math.floor(i / 4 / 28)
       newImg[y][x] = 255 - d[i]
@@ -85,7 +85,8 @@ const App = () => {
   const [image, setImage] = useState(EMPTY)
   const [res, setRes] = useState<number[]>([])
   useEffect(() => {
-    const img = new Tensor([[image]])
+    if (is_eq(image, EMPTY)) return
+    const img = new Tensor([[Array(28).fill(0).map(() => Array(28).fill(0))]])
     mnist.call(img).tolist().then((res) => setRes(res))
   }, [image])
   return (
