32f09fe4173d28277b9c953cc8aa918c93ade506
Karel <nagelkarel@gmail.com>
2025-01-17 08:31:23 +0200

    multi

---
+++ denograd/multi.ts
@@ -0,0 +1,8 @@
+import { MathTrait } from './ops.ts'
+
+export class MultiLazyBuffer extends MathTrait<MultiLazyBuffer> {
+  constructor() {
+    super()
+    throw new Error('not implemented')
+  }
+}
