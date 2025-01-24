b83c5739c35fcb08eb96d0e009573734e76a70dd
Karel <nagelkarel@gmail.com>
2025-01-24 10:10:22 +0200

    save

--- test/engine/memory.test.ts
+++ test/engine/memory.test.ts
@@ -1,11 +1,4 @@
-import { Buffer } from '../../denograd/device.ts'
-import { dtypes } from '../../denograd/dtype.ts'
 import { memory_planner } from '../../denograd/engine/memory.ts'
-import { ScheduleItem } from '../../denograd/engine/schedule.ts'
-import { Metadata } from '../../denograd/helpers.ts'
-import { Ops, UOp } from '../../denograd/ops.ts'
-import { ShapeTracker } from '../../denograd/shape/shapetracker.ts'
-import { View } from '../../denograd/shape/view.ts'
 import { compare } from '../helpers.ts'
 
 Deno.test(
