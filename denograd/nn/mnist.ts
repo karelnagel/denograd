17e802b2c97b5fdcc770d3a63c811c591c9f1950
Karel <nagelkarel@gmail.com>
2025-02-09 20:28:48 +0200

    removed models package

--- denograd/nn/mnist.ts
+++ denograd/nn/mnist.ts
@@ -1,7 +1,12 @@
 // model based off https://towardsdatascience.com/going-beyond-99-mnist-handwritten-digits-recognition-cfff96337392
 
+import { get_number_env, range } from '../helpers.ts'
 import { type Layer, Tensor } from '../tensor.ts'
+import { Tqdm } from '../tqdm.ts'
+import { mnist } from './datasets.ts'
 import { BatchNorm, Conv2d, Linear, Model } from './index.ts'
+import { Adam } from './optim.ts'
+import { get_parameters } from './state.ts'
 
 export class MNIST extends Model {
   override DEFAULT_LOAD = 'https://denograd.com/mnist.safetensors'
@@ -22,3 +27,36 @@ export class MNIST extends Model {
     new Linear(576, 10),
   ]
 }
+
+const main = async () => {
+  const [X_train, Y_train, X_test, Y_test] = await mnist(undefined)
+  const BS = get_number_env('BS', 512)
+
+  const model = new MNIST()
+
+  // await model.load('./mnist.safetensors').catch((x) => x)
+  const opt = Adam(get_parameters(model))
+
+  const train_step = async (): Promise<Tensor> => {
+    Tensor.training = true
+    opt.zero_grad()
+    const samples = Tensor.randint([BS], undefined, X_train.shape[0])
+    const loss = model.call(X_train.get(samples)).sparse_categorical_crossentropy(Y_train.get(samples)).backward()
+    await opt.step()
+    Tensor.training = false
+    return loss
+  }
+
+  const get_test_acc = (): Tensor => model.call(X_test).argmax(1).eq(Y_test).mean().mul(100)
+
+  let test_acc = NaN
+  const t = new Tqdm(range(get_number_env('STEPS', 70)))
+  for await (const i of t) {
+    const loss = await (await train_step()).item()
+    if (i % 10 === 9) test_acc = await get_test_acc().item()
+    t.set_description(`loss: ${loss.toFixed(2)}, test_accuracy: ${test_acc.toFixed(2)}`)
+  }
+  await model.save('./mnist.safetensors')
+}
+
+if (import.meta.main) main()
--- models/mod.ts
+++
@@ -1,1 +0,0 @@
-export * from '../denograd/nn/mnist.ts'
