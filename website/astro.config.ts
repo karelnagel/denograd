9dfe0e7293414e3e4fbd55d773c4aed61e0e8873
Karel <nagelkarel@gmail.com>
2025-02-14 18:28:13 +0200

    added html example

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -4,7 +4,7 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://denograd.io',
+  site: 'https://denograd.com',
   output: 'static',
   integrations: [react(), tailwind()],
   devToolbar: { enabled: false },
