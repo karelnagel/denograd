4ba99a3575b39a25e7528a6e496c981c962c4af0
Karel <nagelkarel@gmail.com>
2025-03-29 18:50:26 +0200

    generate

--- website/src/helpers.ts
+++ website/src/helpers.ts
@@ -6,3 +6,14 @@ export const getExamples = async () => {
   const examples = await fs.readdir('src/examples')
   return await Promise.all(examples.map(async (file) => ({ file, code: await getExample(file) })))
 }
+
+export type JSDoc = { doc: string }
+export type Location = { filename: string; line: number; col: number; byteIndex: number }
+export type ReferenceDef = { target: Location }
+export type Element = { name: string; isDefault: boolean; location: Location; declarationKind: string; kind: string; jsDoc?: JSDoc; reference_def?: ReferenceDef }
+export type NamespaceDef = { elements: Element[] }
+export type Node = { name: string; isDefault: boolean; location: Location; declarationKind: string; kind: string; namespaceDef?: NamespaceDef }
+export type Doc = { version: number; nodes: Node[] }
+export const getDocs = async () => {
+  return JSON.parse(bytes_to_string(await fs.readFile('.astro/docs.json'))) as Doc
+}
